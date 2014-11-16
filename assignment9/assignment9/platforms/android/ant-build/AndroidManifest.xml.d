f1d7e99632410e4bbb7a0c5bbd7275260d6b2639
Edward Lambke <elambke@yahoo.com>
2014-11-15 21:44:58 -0800

    assignment 9 refactor

--- assignment9/assignment9/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/elambke/Documents/COMP2052/comp2052assignments/assignment9/assignment9/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/elambke/Documents/COMP2052/comp2052assignments/assignment9/assignment9/platforms/android/AndroidManifest.xml \
--- assignment9/assignment9/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/elambke/Documents/COMP2052/comp2052assignments/assignment9/assignment9/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/Users/elambke/Documents/COMP2052/comp2052assignments/assignment9/assignment9/platforms/android/ant-build/CordovaApp.ap_ \
-/Users/elambke/Documents/COMP2052/comp2052assignments/assignment9/assignment9/platforms/android/ant-build/classes.dex \
-/Users/elambke/Documents/COMP2052/comp2052assignments/assignment9/assignment9/platforms/android/CordovaLib/ant-build/classes.jar \
